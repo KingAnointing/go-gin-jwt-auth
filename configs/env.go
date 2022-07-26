368c60fcb52f3b70903e815f865e95af3ddb9916
KingAnointing <adetoyeanointing15@gmail.com>
2022-07-26 22:51:19 +0100

    created function to get port address from .env file for port connection

--- configs/env.go
+++ configs/env.go
@@ -7,8 +7,21 @@ import (
 
 	"github.com/joho/godotenv"
 )
+
 // get database from .env file
 func DatabaseURI() string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Unable to retrieve databasef from .env file")
+		log.Fatal(err)
+	}
+
+	return os.Getenv("DATABASEURI")
+}
+
+// get port address to use for connection
+
+func ConnectionPort() string {
 	err := godotenv.Load(".env")
 	if err != nil {
 		fmt.Println("Unable to retrieve databasef from .env file")
