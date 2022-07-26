781a417e6e57e5456540ab3fbd12815bb018d134
KingAnointing <adetoyeanointing15@gmail.com>
2022-07-26 23:11:40 +0100

    created a mockup function to test if auth router is working

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,1 +1,17 @@
 package controllers
+
+import (
+	"net/http"
+
+	"github.com/KingAnointing/go-gin-jwt-project/responses"
+	"github.com/gin-gonic/gin"
+)
+
+// greeter function to test API-1
+func Greeter1() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, responses.Response{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": "Hello from API auth router"}})
+	}
+}
+
+// greeter function to test API-2
