c97871832205c6254a4dd078d3eb2291ca7a03ec
KingAnointing <adetoyeanointing15@gmail.com>
2022-07-27 21:27:14 +0100

    create new mongoDB database client

--- configs/database.go
+++ configs/database.go
@@ -3,9 +3,15 @@ package configs
 import (
 	"context"
 	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DatabaseConnection() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
+	// create client for database
+	mongo.NewClient(options.Client().ApplyURI(DatabaseURI()))
 }
