d3103f27710f0844bfa895de0acfdbf6b1a42084
KingAnointing <adetoyeanointing15@gmail.com>
2022-07-27 23:24:27 +0100

    created a function to generate all token needed,the claims and refreshclaims

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -15,4 +15,22 @@ type SignedDetail struct {
 	jwt.StandardClaims
 }
 
+func GenerateAllToken(firstName string, lastName string, email string, uid string, userType string) (string, string, error) {
+	claims := &SignedDetail{
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		Uid:       uid,
+		UserType:  userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(24 * time.Hour).Unix(),
+		},
+	}
 
+	refreshClaims := &SignedDetail{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(184 * time.Hour).Unix(),
+		},
+	}
+
+}
