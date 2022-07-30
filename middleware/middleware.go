c62b3c861a517ca2da7c5e2bcf2b4519ff9d140c
KingAnointing <adetoyeanointing15@gmail.com>
2022-07-30 21:43:20 +0100

    checked if client token is empty, if empty return error and abort further request

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,9 +1,19 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/KingAnointing/go-gin-jwt-project/responses"
+	"github.com/gin-gonic/gin"
+)
 
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, responses.Response{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": "Unauthorized to access this resources"}})
+			c.Abort()
+			return
+		}
 	}
 }
