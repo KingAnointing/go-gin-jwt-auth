9b28a4410d19b84001295eb7f64eb522ecb5c495
KingAnointing <adetoyeanointing15@gmail.com>
2022-07-30 21:18:11 +0100

    handled error from aggregating the collection

--- controllers/controller.go
+++ controllers/controller.go
@@ -211,5 +211,10 @@ func GetUsers() gin.HandlerFunc {
 		groupStage := bson.D{{"$group", bson.D{{"_id", bson.D{{"_id", "null"}}}, {"total_count", bson.D{{"$sum", 1}}}, {"data", bson.D{{"$push", "$ROOT"}}}}}}
 		projectStage := bson.D{{"$project", bson.D{{"_id", 0}, {"total_count", 1}, {"user_items", bson.D{{"slice", []interface{}{"$data", startIndex, recordPerPage}}}}}}}
 		result, err := collections.Aggregate(ctx, mongo.Pipeline{matchStage, groupStage, projectStage})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, responses.Response{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}})
+			return
+		}
 	}
 }
