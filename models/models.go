ccac29c087e9cdef954d6cc9eb14b61edd2be156
KingAnointing <adetoyeanointing15@gmail.com>
2022-07-30 22:43:12 +0100

    fixed validation err in user_type field in user models struct

--- models/models.go
+++ models/models.go
@@ -13,7 +13,7 @@ type User struct {
 	Email        *string            `json:"email" validate:"required,email"`
 	Password     *string            `json:"password" validate:"required,min=6"`
 	Phone        *string            `json:"phone" validate:"required"`
-	UserType     *string            `json:"user_type" validate:"required,eq=ADMIN|USER"`
+	UserType     *string            `json:"user_type" validate:"required,eq=ADMIN|eq=USER"`
 	Token        *string            `json:"token"`
 	RefreshToken *string            `json:"refresh_token"`
 	CreatedAt    time.Time          `json:"created_at"`
